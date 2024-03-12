739114959cf24954d5eeaa638e4d66ae52b511ce
daniilak <daniilakk@gmail.com>
2024-03-12 22:34:45 +0300

    Added exceptions, fixed class fields

--- ru_mvd_search_wanted/asynchr.py
+++ ru_mvd_search_wanted/asynchr.py
@@ -2,7 +2,8 @@
 import httpx, re
 from bs4 import BeautifulSoup
 from base64 import b64encode
-from ru_mvd_search_wanted.settings import HEADERS, BASE_URL, CAPTCHA_URL
+from ru_mvd_search_wanted.settings import HEADERS, BASE_URL, CAPTCHA_URL, TIMEOUT_SEC
+from exceptions import MVDParserException, MVDParserAPIStatusException
 
 
 class MVDParserAsync:
@@ -23,6 +24,7 @@ class MVDParserAsync:
         day: str,
         mail: str,
         proxy: str = "",
+        
     ) -> None:
         """
         Initializes with person\'s data and session details.
@@ -46,7 +48,7 @@ class MVDParserAsync:
         """
         Initializes the HTTP client session when entering the context.
         """
-        self.client = httpx.AsyncClient(proxies=self.proxy)
+        self.client = httpx.AsyncClient(proxies=self.proxy, timeout=TIMEOUT_SEC)
         return self
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
@@ -56,13 +58,13 @@ class MVDParserAsync:
         await self.client.aclose()
 
     @staticmethod
-    def encode_to_base64(content) -> str:
+    def _encode_to_base64(content) -> str:
         """
         Encodes content to base64.
         """
         return b64encode(content).decode("ascii")
 
-    async def get_csrf_token(self, html_str) -> str:
+    async def _get_csrf_token(self, html_str) -> str:
         """
         Extracts CSRF token from HTML.
         """
@@ -71,7 +73,7 @@ class MVDParserAsync:
         )
         return match.group(1) if match else ""
 
-    async def fetch_get_request(self, url: str, headers) -> httpx.Response:
+    async def _fetch_get_request(self, url: str, headers) -> httpx.Response:
         """
         Fetches a GET request asynchronously and returns the response. Logs error on failure.
         """
@@ -79,10 +81,13 @@ class MVDParserAsync:
             response = await self.client.get(url, headers=headers)
             response.raise_for_status()
         except httpx.RequestError as e:
-            raise e
+            raise MVDParserAPIStatusException.from_response(
+                response,
+                message=f"Status: {response.status_code}. Message: {response.text}",
+            )
         return response
 
-    async def fetch_post_request(self, url: str, params, headers) -> httpx.Response:
+    async def _fetch_post_request(self, url: str, params, headers) -> httpx.Response:
         """
         Fetches a POST request asynchronously and returns the response. Logs error on failure.
         """
@@ -90,19 +95,22 @@ class MVDParserAsync:
             response = await self.client.post(url, headers=headers, data=params)
             response.raise_for_status()
         except httpx.RequestError as e:
-            raise e
+            raise MVDParserAPIStatusException.from_response(
+                response,
+                message=f"Status: {response.status_code}. Message: {response.text}",
+            )
         return response
 
     async def initialize(self) -> str:
         """
         Initializes by asynchronously obtaining tokens and captcha.
         """
-        response = await self.fetch_get_request(BASE_URL, HEADERS["h1"])
-        csrf_token = await self.get_csrf_token(response.text)
+        response = await self._fetch_get_request(BASE_URL, HEADERS["h1"])
+        csrf_token = await self._get_csrf_token(response.text)
         self.params["sec_csrftoken"] = csrf_token
 
-        response = await self.fetch_get_request(CAPTCHA_URL, HEADERS["h2"])
-        captcha_base64 = self.encode_to_base64(response.content)
+        response = await self._fetch_get_request(CAPTCHA_URL, HEADERS["h2"])
+        captcha_base64 = self._encode_to_base64(response.content)
 
         return captcha_base64
 
@@ -111,17 +119,17 @@ class MVDParserAsync:
         Submits the query parameters to the website.
         """
         self.params["captcha"] = captcha_word
-        response = await self.fetch_post_request(BASE_URL, self.params, HEADERS["h3"])
+        response = await self._fetch_post_request(BASE_URL, self.params, HEADERS["h3"])
 
         # Handle various response cases
         if "Введен неверный код с изображения" in response.text:
-            return {"error": "Incorrect captcha code entered"}
+            raise MVDParserException(message="Incorrect captcha code entered")
 
         if "По вашему запросу нет информации" in response.text:
             return {"result": None}
 
         if "можно отправить запрос не более 1 раза в 5 минут" in response.text:
-            return {"error": "Requests can be made no more than once every 5 minutes"}
+            raise MVDParserException(message="Requests can be made no more than once every 5 minutes")
 
         html = BeautifulSoup(response.text, features="lxml")
         results = html.find("div", {"class": "bs-item clearfix"})
--- ru_mvd_search_wanted/sync.py
+++ ru_mvd_search_wanted/sync.py
@@ -2,8 +2,8 @@
 import requests, re
 from bs4 import BeautifulSoup
 from base64 import b64encode
-from ru_mvd_search_wanted.settings import HEADERS, BASE_URL, CAPTCHA_URL
-
+from ru_mvd_search_wanted.settings import HEADERS, BASE_URL, CAPTCHA_URL, TIMEOUT_SEC
+from exceptions import MVDParserException, MVDParserAPIStatusException
 
 class MVDParser:
     """
@@ -42,7 +42,7 @@ class MVDParser:
         # Initializes the HTTP client session when entering the context.
         self.client = requests.Session()
         if self.proxy:
-            self.client.proxies.update(self.proxy)
+            self.client.proxies.update(self.proxy, timeout=TIMEOUT_SEC)
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
@@ -50,59 +50,65 @@ class MVDParser:
         self.client.close()
 
     @staticmethod
-    def encode_to_base64(content) -> str:
+    def _encode_to_base64(content) -> str:
         # Encodes content to base64.
         return b64encode(content).decode("ascii")
 
-    def get_csrf_token(self, html_str) -> str:
+    def _get_csrf_token(self, html_str) -> str:
         # Extracts CSRF token from HTML.
         match = re.search(
             r"<meta name=\'csrf-token-value\' content=\'(.*?)\'/>", html_str
         )
         return match.group(1) if match else ""
 
-    def fetch_get_request(self, url: str, headers) -> requests.Response:
+    def _fetch_get_request(self, url: str, headers) -> requests.Response:
         # Fetches a GET request synchronously and returns the response. Logs error on failure.
         try:
             response = self.client.get(url, headers=headers)
             response.raise_for_status()
         except requests.RequestException as e:
-            raise e
+            raise MVDParserAPIStatusException.from_response(
+                response,
+                message=f"Status: {response.status_code}. Message: {response.text}",
+            )
         return response
 
-    def fetch_post_request(self, url: str, params, headers) -> requests.Response:
+    def _fetch_post_request(self, url: str, params, headers) -> requests.Response:
         # Fetches a POST request synchronously and returns the response. Logs error on failure.
         try:
             response = self.client.post(url, headers=headers, data=params)
             response.raise_for_status()
         except requests.RequestException as e:
-            raise e
+            raise MVDParserAPIStatusException.from_response(
+                response,
+                message=f"Status: {response.status_code}. Message: {response.text}",
+            )
         return response
 
     def initialize(self) -> str:
         # Initializes by synchronously obtaining tokens and captcha.
-        csrf_token = self.get_csrf_token(self.fetch_get_request(BASE_URL, HEADERS["h1"]).text)
+        csrf_token = self._get_csrf_token(self._fetch_get_request(BASE_URL, HEADERS["h1"]).text)
         self.params["sec_csrftoken"] = csrf_token
 
-        response = self.fetch_get_request(CAPTCHA_URL, HEADERS["h2"])
-        captcha_base64 = self.encode_to_base64(response.content)
+        response = self._fetch_get_request(CAPTCHA_URL, HEADERS["h2"])
+        captcha_base64 = self._encode_to_base64(response.content)
 
         return captcha_base64
 
     def get_result(self, captcha_word: str) -> dict:
         # Submits the query parameters to the website.
         self.params["captcha"] = captcha_word
-        response = self.fetch_post_request(BASE_URL, self.params, HEADERS["h3"])
+        response = self._fetch_post_request(BASE_URL, self.params, HEADERS["h3"])
 
         # Handle various response cases
         if "Введен неверный код с изображения" in response.text:
-            return {"error": "Incorrect captcha code entered"}
+            raise MVDParserException(message="Incorrect captcha code entered")
 
         if "По вашему запросу нет информации" in response.text:
             return {"result": None}
 
         if "можно отправить запрос не более 1 раза в 5 минут" in response.text:
-            return {"error": "Requests can be made no more than once every 5 minutes"}
+            raise MVDParserException(message="Requests can be made no more than once every 5 minutes")
 
         html = BeautifulSoup(response.text, features="lxml")
         results = html.find("div", {"class": "bs-item clearfix"})
